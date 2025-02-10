bca47fe04a3e0374f17b379c5c8023815f331b45
delorency <delorency>
2025-02-10 06:54:38 +0300

    database fix

--- database/database.go
+++ database/database.go
@@ -17,8 +17,13 @@ type Storage struct {
 
 func InitDB() (*Storage, error) {
 	godotenv.Load()
-
-	conn := os.Getenv("conn")
+	conn := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASS"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_NAME"),
+	)
 
 	db, err := sql.Open("postgres", conn)
 	if err != nil {
